5f64bebe4459b0015c45b2475e55801d5427967f
lekssss <lekhashanthinir@gmail.com>
2020-08-24 23:19:23 +0530

    Registered models in admin

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Category, Items, Toppings, Pricing
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Items)
+admin.site.register(Toppings)
+admin.site.register(Pricing)
--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Category
 
 # Create your views here.
 def index(request):
-    return render(request,"menu/index.html")
+    return render(request, "menu/index.html", {
+        "categories": Category.objects.all()
+    })
